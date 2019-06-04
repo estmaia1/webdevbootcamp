a7202d901874c39af5c57488c1e162a38d3bf14b
estmaia1 <estmaia@gmail.com>
2019-06-04 19:18:53 -0300

    Updated AuthDemo app.js with MongoDB

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,25 +1,25 @@
 var express = require("express");
 var mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://devstvn:<>@cluster0-yntnc.mongodb.net/test?retryWrites=true&w=majority", {
-	useNewUrlParser: true,
-	useCreateIndex: true
-}).then(() =>{
-	console.log("Connected to DB!");
+mongoose.connect("mongodb+srv://devstvn:devstvn123@cluster0-yntnc.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => {
+  console.log("Connected to DB!");
 }).catch(err => {
-	console.log("ERROR:", err.message);
+  console.log("ERROR:", err.message);
 });
 
 var app = express();
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-	res.render("home");
+app.get("/", function(req, res) {
+  res.render("home");
 });
 
-app.get("/secret", function(req, res){
-	res.render("secret");
+app.get("/secret", function(req, res) {
+  res.render("secret");
 });
 
-app.listen(3000, () => {
-	console.log("Server started..........");
+app.listen(8000, '127.0.0.1', function(req, res) {
+  console.log("Server started..........");
 });
