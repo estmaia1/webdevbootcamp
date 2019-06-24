cb4522f1751ad0fc1bdec8343d24b05e6f92340f
estmaia1 <estmaia@gmail.com>
2019-06-24 07:35:56 -0300

    Finished implementing flash messages and handled some route errors

--- yelpcamp/middleware/index.js
+++ yelpcamp/middleware/index.js
@@ -7,7 +7,8 @@ var middlewareObj = {};
 middlewareObj.checkCampgroundOwnership = function(req, res, next) {
 	if (req.isAuthenticated()) {
 		Campground.findById(req.params.id, function(err, foundCampground) {
-			if (err) {
+			if (err || !foundCampground) {
+				req.flash("error", "Campground not found");
 				res.redirect("back");
 			} else {
 				// does user own the campground?
@@ -15,12 +16,14 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
 					next();
 				} else {
 					//otherwise, redirect
+					req.flash("error", "You don't have permission to do that");
 					res.redirect("back");
 				}
 			}
 		});
 	} else {
 		// if not, redirect
+		req.flash("error", "You need to be logged in to do that");
 		res.redirect("back");
 	};
 };
@@ -28,7 +31,8 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
 middlewareObj.checkCommentOwnership = function(req, res, next) {
 	if (req.isAuthenticated()) {
 		Comment.findById(req.params.comment_id, function(err, foundComment) {
-			if (err) {
+			if (err || !foundComment) {
+				req.flash("error", "Comment not found");
 				res.redirect("back");
 			} else {
 				// does user own the comment?
@@ -36,12 +40,14 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
 					next();
 				} else {
 					//otherwise, redirect
+					req.flash("error", "You don't have permission to do that");
 					res.redirect("back");
 				}
 			}
 		});
 	} else {
 		// if not, redirect
+		req.flash("error", "You need to be logged in to do that");
 		res.redirect("back");
 	};
 };
@@ -50,7 +56,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
 	}
-	req.flash("error", "Please Login First!");
+	req.flash("error", "You need to be logged in to do that");
 	res.redirect("/login");
 };
 
--- yelpcamp/routes/comments.js
+++ yelpcamp/routes/comments.js
@@ -27,6 +27,7 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
 			// create new comment
 			Comment.create(req.body.comment, function(err, comment) {
 				if (err) {
+					req.flash("error", "Something went wrong");
 					console.log(err);
 				} else {
 					// add username and id to comment
@@ -38,6 +39,7 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
 					campground.comments.push(comment);
 					campground.save();
 					// redirect to campground showpage
+					req.flash("success", "Successfully added comment");
 					res.redirect("/campgrounds/" + campground._id);
 				}
 			});
@@ -47,12 +49,18 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
 
 // COMMENT EDIT ROUTE
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res) {
-	Comment.findById(req.params.comment_id, function(err, foundComment) {
-		if (err) {
-			res.redirect("back");
-		} else {
-			res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if (err || !foundCampground) {
+			req.flash("error", "Campground not found");
+			return res.redirect("back");
 		}
+		Comment.findById(req.params.comment_id, function(err, foundComment) {
+			if (err) {
+				res.redirect("back");
+			} else {
+				res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+			}
+		});
 	});
 });
 
@@ -73,6 +81,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
 		if (err) {
 			res.redirect("back");
 		} else {
+			req.flash("success", "Comment deleted");
 			res.redirect("/campgrounds/" + req.params.id);
 		}
 	});
