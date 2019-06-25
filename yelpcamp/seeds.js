fb54a50ccd9c565d316b043b9620eb0871ca26f0
estmaia1 <estmaia@gmail.com>
2019-06-25 08:57:37 -0300

    Added dynamic pricing feature

--- yelpcamp/seeds.js
+++ yelpcamp/seeds.js
@@ -4,6 +4,7 @@ var Comment = require("./models/comment");
 
 var data = [{
         name: "Cloud's Rest",
+        price: "9.50",
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         author: {
@@ -13,6 +14,7 @@ var data = [{
     },
     {
         name: "Desert Mesa",
+        price: "10.00",
         image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         author: {
@@ -22,11 +24,12 @@ var data = [{
     },
     {
         name: "Canyon Floor",
+        price: "8.75",
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         author: {
-            id: "5d076da5a6a5f4089eaf6309",
-            username: "test"
+            id: "5d12042eca4dd609482320b6",
+            username: "Steven"
         }
     }
 ];
@@ -54,8 +57,8 @@ function seedDB() {
                         Comment.create({
                             text: "This place is great, but I wish there was internet",
                             author: {
-                                id: "5d076da5a6a5f4089eaf6309",
-                                username: "test"
+                                id: "5d12042eca4dd609482320b6",
+                                username: "Steven"
                             }
                         }, function(err, comment) {
                             if (err) {
@@ -75,68 +78,3 @@ function seedDB() {
 }
 
 module.exports = seedDB;
-
-// var mongoose = require("mongoose");
-// var Campground = require("./models/campground");
-// var Comment   = require("./models/comment");
-
-// var data = [
-//     {
-//         name: "Cloud's Rest", 
-//         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-//     },
-//     {
-//         name: "Desert Mesa", 
-//         image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-//     },
-//     {
-//         name: "Canyon Floor", 
-//         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-//     }
-// ]
-
-// function seedDB(){
-//    //Remove all campgrounds
-//    Campground.deleteMany({}, function(err){
-//         if(err){
-//             console.log(err);
-//         }
-//         console.log("removed campgrounds!");
-//         Comment.deleteMany({}, function(err) {
-//             if(err){
-//                 console.log(err);
-//             }
-//             console.log("removed comments!");
-//              //add a few campgrounds
-//             data.forEach(function(seed){
-//                 Campground.create(seed, function(err, campground){
-//                     if(err){
-//                         console.log(err);
-//                     } else {
-//                         console.log("added a campground");
-//                         //create a comment
-//                         Comment.create(
-//                             {
-//                                 text: "This place is great, but I wish there was internet",
-//                                 author: "Homer"
-//                             }, function(err, comment){
-//                                 if(err){
-//                                     console.log(err);
-//                                 } else {
-//                                     campground.comments.push(comment);
-//                                     campground.save();
-//                                     console.log("Created new comment");
-//                                 }
-//                             });
-//                     }
-//                 });
-//             });
-//         });
-//     }); 
-//     //add a few comments
-// }
-
-// module.exports = seedDB;
